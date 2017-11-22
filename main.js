d46ce96f32454d2c9e1c5acca5a9dd4b091adc34
Andreas Hocevar <andreas.hocevar@gmail.com>
2017-11-22 20:44:51 +0100

    New version without style.json

--- main.js
+++ main.js
@@ -1,73 +1,64 @@
 import 'ol/ol.css';
-import 'javascript-autocomplete/auto-complete.css';
-import GeoJSON from 'ol/format/geojson';
-import Overlay from 'ol/overlay';
+import Map from 'ol/map';
+import View from 'ol/view';
+import TileLayer from 'ol/layer/tile';
+import Stamen from 'ol/source/stamen';
 import VectorLayer from 'ol/layer/vector';
-import VectorSource from 'ol/source/vector';
+import Vector from 'ol/source/vector';
+import GeoJSON from 'ol/format/geojson';
+import Style from 'ol/style/style';
+import Circle from 'ol/style/circle';
+import Fill from 'ol/style/fill';
+import Stroke from 'ol/style/stroke';
+import Overlay from 'ol/overlay';
 import proj from 'ol/proj';
 import sync from 'ol-hashed';
-import {apply} from 'ol-mapbox-style';
-import AutoComplete from 'javascript-autocomplete';
 
-var onload, selected, source;
+const map = new Map({
+  target: 'map',
+  view: new View({
+    center: proj.fromLonLat([16.37, 48.2]),
+    zoom: 13
+  })
+});
+map.addLayer(new TileLayer({
+  source: new Stamen({
+    layer: 'watercolor'
+  })
+}));
 
-var map = apply('map-container', 'data/style.json');
-sync(map);
+const bezirkeLayer = new VectorLayer({
+  source: new Vector({
+    url: 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json',
+    format: new GeoJSON()
+  })
+});
+map.addLayer(bezirkeLayer);
 
-function fit() {
-  map.getView().fit(source.getExtent(), {
-    maxZoom: 19,
-    duration: 250
+const layer = new VectorLayer({
+  source: new Vector({
+    url: 'https://student.ifip.tuwien.ac.at/geoweb/2017/ifip/map/postgis_geojson.php',
+    format: new GeoJSON()
+  })
+});
+map.addLayer(layer);
+
+layer.setStyle(function(feature) {
+  return new Style({
+    image: new Circle({
+      radius: 7,
+      fill: new Fill({
+        color: 'rgba(232, 12, 12, 1)'
+      }),
+      stroke: new Stroke({
+        color: 'rgba(127, 127, 127, 1)',
+        width: 1
+      })
+    })
   });
-}
-
-function getAddress(feature) {
-  var properties = feature.getProperties();
-  return (
-    (properties.city || properties.name || '') +
-    ' ' +
-    (properties.street || '') +
-    ' ' +
-    (properties.housenumber || '')
-  );
-}
-
-var searchResult = new VectorLayer({
-  zIndex: 9999
 });
-map.addLayer(searchResult);
 
-new AutoComplete({
-  selector: 'input[name="q"]',
-  source: function(term, response) {
-    if (onload) {
-      source.un('change', onload);
-    }
-    searchResult.setSource(null);
-    source = new VectorSource({
-      format: new GeoJSON(),
-      url: 'https://photon.komoot.de/api/?q=' + term
-    });
-    onload = function(e) {
-      var texts = source.getFeatures().map(function(feature) {
-        return getAddress(feature);
-      });
-      response(texts);
-      fit();
-    };
-    source.once('change', onload);
-    searchResult.setSource(source);
-  },
-  onSelect: function(e, term, item) {
-    selected = item.getAttribute('data-val');
-    source.getFeatures().forEach(function(feature) {
-      if (getAddress(feature) !== selected) {
-        source.removeFeature(feature);
-      }
-    });
-    fit();
-  }
-});
+sync(map);
 
 var overlay = new Overlay({
   element: document.getElementById('popup-container'),
@@ -84,15 +75,15 @@ map.on('singleclick', function(e) {
   var markup = '';
   map.forEachFeatureAtPixel(e.pixel, function(feature) {
     var properties = feature.getProperties();
-    if (properties['f_nr']) {
-      markup += `${markup && '<hr>'}<table>`;
-      for (var property in properties) {
-        if (property != 'geometry') {
-          markup += `<tr><th>${property}</th><td>${properties[property]}</td></tr>`;
-        }
+    markup += `${markup && '<hr>'}<table>`;
+    for (var property in properties) {
+      if (property != 'geometry') {
+        markup += `<tr><th>${property}</th><td>${properties[property]}</td></tr>`;
       }
-      markup += '</table>';
     }
+    markup += '</table>';
+  }, {
+    layerFilter: (l) => l === layer
   });
   if (markup) {
     document.getElementById('popup-content').innerHTML = markup;
@@ -100,34 +91,51 @@ map.on('singleclick', function(e) {
   } else {
     overlay.setPosition();
     var pos = proj.toLonLat(e.coordinate);
-    window.location.href = 'https://student.ifip.tuwien.ac.at/geoweb/2017/ifip/map/feedback.php?pos=' +
+    window.location.href =
+        'https://student.ifip.tuwien.ac.at/geoweb/2017/ifip/map/feedback.php?pos=' +
         pos.join(' ');
   }
 });
 
-var loadedSources = {};
-map.getLayers().on('add', function(e) {
-  var source = e.element.getSource();
-  if (source instanceof VectorSource) {
-    source.once('change', function() {
-      loadedSources[source.getUrl()] = source;
-      if (Object.keys(loadedSources).length == 2) {
-        var feedbacks = loadedSources['https://student.ifip.tuwien.ac.at/geoweb/2017/ifip/map/postgis_geojson.php'].getFeatures();
-        var bezirke = loadedSources['https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BEZIRKSGRENZEOGD&srsName=EPSG:4326&outputFormat=json'].getFeatures();
-        for (var i = 0, ii = feedbacks.length; i < ii; ++i) {
-          var feedback = feedbacks[i];
-          for (var j = 0, jj = bezirke.length; j < jj; ++j) {
-            var bezirk = bezirke[j];
-            var count = bezirk.get('FEEDBACKS') || 0;
-            var feedbackGeom = feedback.getGeometry();
-            if (feedbackGeom &&
-                bezirk.getGeometry().intersectsCoordinate(feedbackGeom.getCoordinates())) {
-              ++count;
-            }
-            bezirk.set('FEEDBACKS', count);
-          }
+function calculateStatistics() {
+  const feedbacks = layer.getSource().getFeatures();
+  const bezirke = bezirkeLayer.getSource().getFeatures();
+  if (feedbacks.length > 0 && bezirke.length > 0) {
+    for (var i = 0, ii = feedbacks.length; i < ii; ++i) {
+      var feedback = feedbacks[i];
+      for (var j = 0, jj = bezirke.length; j < jj; ++j) {
+        var bezirk = bezirke[j];
+        var count = bezirk.get('FEEDBACKS') || 0;
+        var feedbackGeom = feedback.getGeometry();
+        if (feedbackGeom &&
+            bezirk.getGeometry().intersectsCoordinate(feedbackGeom.getCoordinates())) {
+          ++count;
         }
+        bezirk.set('FEEDBACKS', count);
       }
-    });
+    }
   }
+}
+bezirkeLayer.getSource().once('change', calculateStatistics);
+layer.getSource().once('change', calculateStatistics);
+
+bezirkeLayer.setStyle(function(feature) {
+  var fillColor;
+  const feedbackCount = feature.get('FEEDBACKS');
+  if (feedbackCount <= 1) {
+    fillColor = 'rgba(247, 252, 185, 0.7';
+  } else if (feedbackCount < 5) {
+    fillColor = 'rgba(173, 221, 142, 0.7)';
+  } else {
+    fillColor = 'rgba(49, 163, 84, 0.7)';
+  }
+  return new Style({
+    fill: new Fill({
+      color: fillColor
+    }),
+    stroke: new Stroke({
+      color: 'rgba(4, 4, 4, 1)',
+      width: 1
+    })
+  });
 });
